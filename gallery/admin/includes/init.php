6eafc59590036b686b1e26e93726dbd0bee8d760
Miguel <miguelangelramirez@me.com>
2017-02-24 17:19:53 -0500

    db_object.php mysteriously appeared to be empty.

--- gallery/admin/includes/init.php
+++ gallery/admin/includes/init.php
@@ -1,11 +1,9 @@
-<?php 
-	require_once('admin-config.php');
-	require_once('database.php');
-	require_once('user.php');
-	require_once('functions.php');
-	require_once('session.php');
-	require_once('db_object.php');
-
-
+<?php
+    require_once( 'admin-config.php' );
+    require_once( 'database.php' );
+    require_once( 'db_object.php' );
+    require_once( 'user.php' );
+    require_once( 'functions.php' );
+    require_once( 'session.php' );
 
 ?>
